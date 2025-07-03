fca6e78640be325a3cf62faaeea7ce47460f4f2e
Lucaevalexeyskia96aq <Lucaevalexeyskia96aq@gmail.com>
2025-07-03 12:20:37 +0200

    Update Kernel.cuh

--- Kernel.cuh
+++ Kernel.cuh
@@ -24,8 +24,8 @@
 
 #define ALPHABET_LEN 12
 
-const char ALPHABET[13] = "0123456789CM";
-__device__ __constant__ char _ALPHABET[13] = "0123456789CM";
+const char ALPHABET[13] = "0123456789AK";
+__device__ __constant__ char _ALPHABET[13] = "0123456789AK";
 
 cudaError_t loadPrefix(const char* _prefix, size_t const prefixLen);
 cudaError_t loadLevel(int _level);
